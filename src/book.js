c253bde5659f974574640c871eb3eac21389cde4
Sole <barboza.soledad@gmail.com>
2023-09-19 22:35:41 -0300

    Completed testing of the book.js file.

--- src/book.js
+++ src/book.js
@@ -1,12 +1,46 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return "The " + title;
 }
+
+function buildMainCharacter(name, age) {
+  return {
+    name: name,
+    age: age
+  };
+
+}
+
+function saveReview(newReviews, arrayreviews) {
+  if (!arrayreviews.includes(newReviews)) {
+      arrayreviews.push(newReviews);
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(title, mainCharacter, genre) {
+  return {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: title.length * 20,
+    genre: genre
+  }
+
+}
+
+function editBook(book) {
+  book.pageCount = (book.pageCount / 4) * 3;
+}
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
